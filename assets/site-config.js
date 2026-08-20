b682d7ed1f2b8c3b883f683c6696998ec63fc480
RY5K <ryan.skogstad@gmail.com>
2026-08-20 07:41:50 -0500

    Add test site runtime API configuration

---
+++ assets/site-config.js
@@ -0,0 +1,5 @@
+window.FLC_SITE_CONFIG = Object.freeze({
+  environment: "firstlutheranifalls.site-test",
+  apiBase: "https://first-lutheran-site-api.ryan-skogstad.workers.dev",
+  editorUrl: "https://admin.firstlutheranifalls.site/"
+});
